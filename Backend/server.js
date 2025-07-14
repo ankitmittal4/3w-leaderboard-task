505ff72b4a8ae3d42d3d7dcd11730fa6cc7d3d3a
Ankit <ankitmittal0814@gmail.com>
2025-07-14 21:19:40 +0530

    Update cors

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,13 @@ require('dotenv').config();
 const app = express();
 
 connectDB();
-app.use(cors());
+// app.use(cors());
+app.use(
+    cors({
+        origin: '*'
+    })
+);
+
 app.use(express.json());
 app.use('/api', userRoutes);
 
